6a9e12a2bf85b7a23ce6ba0c7ae52835d16717ea
sushanthk-262 <20mcme02@uohyd.ac.in>
2023-04-19 09:58:36 +0530

    added browser use case js code(s)

---
+++ VAN/browser_js/onSelect.js
@@ -0,0 +1,20 @@
+let msg = new SpeechSynthesisUtterance();
+let voices = speechSynthesis.getVoices();
+msg.voice = voices[0];
+let tags = document.querySelectorAll('p,a,h1,h2,h3,button'); // add more tags
+tags.forEach((tag) => {
+    tag.addEventListener('click', (e) => {
+        
+        msg.text = e.target.innerText;
+        tag.style.backgroundColor = "yellow";
+        speechSynthesis.speak(msg);
+        
+        let interval = setInterval(() => {
+            if(!speechSynthesis.speaking){
+                tag.style.removeProperty('background-color');
+                clearInterval(interval);
+            }
+        }, 100);
+        
+    });
+});
---
+++ VAN/browser_js/readEverything.js
@@ -0,0 +1,43 @@
+// Create an object to store the screen reader functionality
+var screenReader = {};
+
+// Function to read the content of an element
+screenReader.readElement = function(element) {
+  // Create a new temporary element to store the text
+  var tempElement = document.createElement("div");
+  tempElement.setAttribute("role", "text");
+
+  // Get the text content of the element
+  var textContent = element.textContent || element.innerText || "";
+
+  // Add the text content to the temporary element
+  tempElement.innerHTML = textContent;
+
+  // Append the temporary element to the body
+  document.body.appendChild(tempElement);
+
+  // Use the text-to-speech API to read the text
+  window.speechSynthesis.speak(new SpeechSynthesisUtterance(textContent));
+
+  // Remove the temporary element from the body
+  document.body.removeChild(tempElement);
+};
+
+// Function to read the content of a page
+screenReader.readPage = function() {
+  // Get all the elements on the page
+  var elements = document.querySelectorAll("*");
+
+  // Loop through each element and read its content
+  for (var i = 0; i < elements.length; i++) {
+    screenReader.readElement(elements[i]);
+  }
+};
+
+// Add a button to the page to trigger the screen reader
+var button = document.createElement("button");
+button.innerHTML = "Read Page";
+button.addEventListener("click", function() {
+  screenReader.readPage();
+});
+document.body.appendChild(button);
